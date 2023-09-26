1cf201ef5f31f1eddba041a1062c5feabb4584b3
Andrés Martínez <sbsmrth>
2023-09-26 16:00:17 -0500

    fix: redirect fixed

--- src/app/authentication-opt-ways/authentication-opt-ways.component.ts
+++ src/app/authentication-opt-ways/authentication-opt-ways.component.ts
@@ -24,7 +24,7 @@ export class AuthenticationOptWaysComponent {
         next: (data) => {
           const url = data.otpAuthURL
 
-          this.router.navigate(['/auth-opt']);
+          this.router.navigate([`/qr-gen/${url}`]);
         },
         error: (error) => {
           console.error('There was an error!', error);
