b03a9e51857ae128fb5223009ca1bf6b31af2e35
SantiagoBedoya <a.sbedoya200110@gmail.com>
2023-10-11 09:08:35 -0500

    feat: find nearest driver on trip request

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -5,9 +5,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RequestTripComponent } from './pages/request-trip/request-trip.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NearestDriverCardComponent } from './components/nearest-driver-card/nearest-driver-card.component';
 
 @NgModule({
-  declarations: [HomeComponent, NavbarComponent, RequestTripComponent],
+  declarations: [HomeComponent, NavbarComponent, RequestTripComponent, NearestDriverCardComponent],
   imports: [CommonModule, ProtectedRoutingModule, ReactiveFormsModule],
 })
 export class ProtectedModule {}
