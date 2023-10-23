95e0de23cce986000e4e3e9998434ba2c0024dac
Andrés Martínez <sbsmrth>
2023-10-22 21:49:52 -0500

    feat: cancel trip

--- src/app/protected/services/trip.service.ts
+++ src/app/protected/services/trip.service.ts
@@ -37,4 +37,13 @@ export class TripService {
       .set('Authorization', `Bearer ${token}`);
     return this.httpClient.get<any>(`${this.uri}/${tripId}/panic`, { headers });
   }
+
+  cancelTrip(tripId: string, trip: any) {
+    const token = sessionStorage.getItem('access_token');
+    const headers = new HttpHeaders()
+      .set('content-type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+    return this.httpClient.patch<any>(`${this.uri}/${tripId}
+    `, trip, { headers });
+  }
 }
