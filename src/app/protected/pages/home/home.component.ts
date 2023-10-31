e0b4560053f0b29430f015406655b9045a86b748
vnsaba <vanesa07alzate@gmail.com>
2023-10-30 19:06:34 -0500

    feat: emit acept trip

--- src/app/protected/pages/home/home.component.ts
+++ src/app/protected/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Permissions } from 'src/app/auth/permissions/permission.enum';
 import { TripService } from '../../services/trip.service';
 import { Trip } from '../../interfaces/trip.interface';
+import { WebsocketService } from '../../services/websocket.service';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -12,16 +14,29 @@ export class HomeComponent {
   createTrip: number = Permissions.CreateTrip;
   listTrips: number = Permissions.ListTrip;
   activeTripId: string = '';
-
-  constructor(
-    private tripsService: TripService,
-  ) {}
-
   trip: Trip | null = null;
   tripStatus: string | undefined = '';
   roleName: string = '';
 
+  @ViewChild('newTrip')
+  public newTrip!: SwalComponent;
+
+  constructor(
+    private tripsService: TripService,
+    private readonly websocketService: WebsocketService
+  ) {}
+
   ngOnInit(): void {
+    this.websocketService.notifications.subscribe((data: any) => {
+      console.log(data);
+      // this.acceptTrip.title = 'Trip - $' + response.price;
+      //     this.acceptTrip.html = `<b>Route: </b>${response.points
+      //       .map((p) => p.name)
+      //       .join(',')}`;
+      this.newTrip.title = data.message;
+      this.newTrip.html = `<b>Route: Client is: ${data.clientId} ¿Do you want to accept this trip?</b>`;
+      this.newTrip.fire();
+    });
     this.tripsService.trips().subscribe({
       next: (data) => {
         this.trip =
@@ -36,5 +51,19 @@ export class HomeComponent {
     });
 
     this.roleName = sessionStorage.getItem('role_name')!;
+  }
+
+  onAcceptTrip() {
+    this.tripsService.trips().subscribe({
+      next: (data) => {
+        const trip =
+          data.find(
+            (trip) => trip.status === 'ACTIVE' || trip.status === 'PENDING'
+          ) ?? null;
+
+        console.log(this.trip?._id);
+        this.websocketService.emit('accept-trip', { tripId: trip?._id });
+      },
+    });
   }
 }
